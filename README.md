4aca2baf2f0260067a63a8a3d69a9bd70e629c85
Daniil <166209591+DaniilAmoshiy1@users.noreply.github.com>
2024-04-30 20:32:33 +0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,21 @@
+# PYGAME SANDBOX
+
+Что то делаю с pygame
+
+### install libraries
+
+```bash
+pip install -r requirements.txt
+```
+
+### run pygame
+
+```bash
+python game/main.py
+```
+
+### build HTML
+
+```bash
+pygbag --build game
+```
---
+++ game/main.py
@@ -0,0 +1,46 @@
+import asyncio
+import sys
+
+import pygame
+
+COLORS = ('yellow', 'blue', 'red')
+FPS = 25
+
+
+async def main():
+    pygame.init()
+    screen: pygame.Surface = pygame.display.set_mode((1920, 1080))
+    clock = pygame.time.Clock()
+
+    color_index = 0
+
+    while True:
+        for event in pygame.event.get():
+            if not event.type == pygame.KEYUP:
+                continue
+
+            if event.key == pygame.K_d:
+                color_index = (color_index + 1) % len(COLORS)
+                new_color = COLORS[color_index]
+                screen.fill(new_color)
+                pygame.display.update()
+
+            if event.key == pygame.K_a:
+                color_index = (color_index - 1) % len(COLORS)
+                new_color = COLORS[color_index]
+                screen.fill(new_color)
+                pygame.display.update()
+
+
+            if event.key == pygame.K_ESCAPE:
+                pygame.quit()
+                sys.exit()
+
+        clock.tick(FPS)
+        await asyncio.sleep(0)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
+
+print('For add commit')
